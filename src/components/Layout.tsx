de73382ecaeb04fd71c565a272f2a4aaf45c0800
Patryk Pilecki <patryk@webjaksklep.eu>
2023-05-02 11:44:19 +0200

    feat: next seo - cleanup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,10 +10,6 @@ interface LayoutProps {
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col justify-center min-h-screen px-4">
-      <Head>
-        <title>Tytuł strony</title>
-        <meta name="description" content="Opis sklepu"></meta>
-      </Head>
       <Header />
       <main className="flex-grow w-full max-w-screen-xl mx-auto grid py-5">
         {children}
